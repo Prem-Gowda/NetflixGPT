e1e856b07293a145473f9e9d8aed3314485ad955
Prem-Gowda <premgowda369@gmail.com>
2025-10-03 15:14:37 +0530

    Sign In & Sign Up Form

---
+++ src/Components/Login.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Header from "./Header";
+
+const Login = () => {
+  const [signInForm, setSignInForm] = useState(true);
+  const toogleSignInForm = () => {
+    setSignInForm(!signInForm);
+  };
+  return (
+    <div className="relative w-full h-screen">
+      <Header />
+      <img
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/d44b8e7c-e52b-45bc-9568-7d009f91c9ee/web/IN-en-20250929-TRIFECTA-perspective_82a31bf9-6c15-4866-9ba4-fed503316d1d_small.jpg"
+        alt="background"
+        className="w-full h-full object-cover"
+      />
+
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+
+      <div className="absolute inset-0 flex items-center justify-center">
+        <form className="bg-black bg-opacity-70 p-8 rounded-lg w-80">
+          <h2 className="text-white text-2xl font-bold mb-6">
+            {signInForm ? "Sign In" : "Sign Up"}
+          </h2>
+
+          {!signInForm && (
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="w-full p-3 mb-4 rounded bg-gray-800 text-white placeholder-gray-400 outline-none"
+            />
+          )}
+
+          <input
+            type="text"
+            placeholder="Email Address"
+            className="w-full p-3 mb-4 rounded bg-gray-800 text-white placeholder-gray-400 outline-none"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full p-3 mb-4 rounded bg-gray-800 text-white placeholder-gray-400 outline-none"
+          />
+
+          <button
+            type="submit"
+            className="w-full bg-red-600 hover:bg-red-700 text-white p-3 rounded font-semibold"
+          >
+            {signInForm ? "Sign In" : "Sign Up"}
+          </button>
+          <p
+            className="text-white py-4 cursor-pointer"
+            onClick={toogleSignInForm}
+          >
+            {signInForm
+              ? "New to Netflix? Sign In Now"
+              : "Already Registered? Sign In"}
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
